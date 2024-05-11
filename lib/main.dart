008e9766f5eeb45eeb387315af763ee1f9fe0895
YIZZIA_AMAHIRANI_MONGE_MANCINAS <a.21308051280509@cbtis128.edu.mx>
2024-05-11 01:01:50 +0000

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,34 +8,154 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Veterinaria 0509',
+      home: const MyHomePage(),
+      theme: ThemeData(colorSchemeSeed: Colors.white),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String selectedAnimal = "Ninguno";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        backgroundColor: Colors.teal,
+        title: const Text("Veterinaria Monge 0509",
+            style: TextStyle(color: Colors.white, fontSize: 25)),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Animal Seleccionado: $selectedAnimal",
+              style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 25),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 300,
+              child: RadioMenuButton(
+                value: "Perros",
+                groupValue: selectedAnimal,
+                onChanged: (selectedValue) {
+                  setState(() => selectedAnimal = selectedValue!);
+                },
+                style: ButtonStyle(
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+                  elevation: const MaterialStatePropertyAll(2),
+                  backgroundColor:
+                      const MaterialStatePropertyAll(Color(0xff14be55)),
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      height: 50,
+                      child: Image.asset("assets/images/dog.png"),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text(
+                      "Perros",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 10,
+              height: 20,
+            ),
+            SizedBox(
+              width: 300,
+              child: RadioMenuButton(
+                value: "Gatos",
+                groupValue: selectedAnimal,
+                onChanged: (selectedValue) {
+                  setState(() => selectedAnimal = selectedValue!);
+                },
+                style: ButtonStyle(
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+                  elevation: const MaterialStatePropertyAll(2),
+                  backgroundColor:
+                      const MaterialStatePropertyAll(Color(0xff159a6d)),
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      height: 50,
+                      child: Image.asset("assets/images/cat.png"),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text(
+                      "Gatos",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 300,
+              child: RadioMenuButton(
+                value: "Aves",
+                groupValue: selectedAnimal,
+                onChanged: (selectedValue) {
+                  setState(() => selectedAnimal = selectedValue!);
+                },
+                style: ButtonStyle(
+                  shape: MaterialStatePropertyAll(RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10))),
+                  elevation: const MaterialStatePropertyAll(2),
+                  backgroundColor:
+                      const MaterialStatePropertyAll(Color(0xff1b6997)),
+                ),
+                child: Row(
+                  children: [
+                    const SizedBox(width: 10),
+                    SizedBox(
+                      height: 50,
+                      child: Image.asset("assets/images/bird.png"),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text(
+                      "Aves",
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
         ),
       ),
     );
